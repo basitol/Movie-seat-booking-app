c7a51d045823d530344c641fe8f99c72fae23d04
basitol <41623944+basitol@users.noreply.github.com>
2022-03-28 07:28:57 +0100

    saving to the localStorage

--- index.js
+++ index.js
@@ -6,19 +6,39 @@ const movieSelect = document.querySelector("#movie");
 
 let ticketPrice = +movie.value;
 
+// save selected movie data
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedPriceIndex", moviePrice);
+};
+
+// Update total and count
 const setCount = (e) => {
   const selectedSeat = document.querySelectorAll(".row .seat.selected");
+
+  const seatIndex = [...selectedSeat].map((item) => {
+    return [...seats].indexOf(item);
+  });
+
+  localStorage.setItem("selectedSeat", JSON.stringify(seatIndex));
+
   count.innerText = selectedSeat.length;
   total.innerText = ticketPrice * selectedSeat.length;
 };
 
+// Update when movie is changed
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+
+  setMovieData(e.target.selectedIndex, e.target.value);
+  //   let movieIndex = e.target.selectedIndex;
+
+  //   localStorage.setItem("movieIndex", movieIndex);
 
   setCount();
 });
 
-//
+// Select seats
 container.addEventListener("click", (e) => {
   if (
     e.target.classList.contains("seat") &&
